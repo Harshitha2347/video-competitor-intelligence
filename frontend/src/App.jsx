523188c6e090793343ac4fa6f57db35f6a5ac911
Harshitha <harshithababu2316@gmail.com>
2026-05-20 22:27:19 +0530

    fixed ppt download url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
       setError("")
       setReportData(null)
 
-      const response=await fetch("http://127.0.0.1:8000/analyze",{
+      const response=await fetch("https://video-competitor-intelligence-i31j.onrender.com",{
         method:"POST",
         headers:{
           "Content-Type":"application/json"
